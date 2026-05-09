64ce9cbdb31cc20d7f4aa69ad4edf7d3b508e80d
syk8015 <lwk207088@gmail.com>
2026-05-09 16:00:16 +0900

    Allow all external image hostnames in next.config

--- next.config.ts
+++ next.config.ts
@@ -6,6 +6,8 @@ const nextConfig: NextConfig = {
       { protocol: "https", hostname: "i.pravatar.cc" },
       { protocol: "https", hostname: "picsum.photos" },
       { protocol: "https", hostname: "fastly.picsum.photos" },
+      { protocol: "https", hostname: "*.supabase.co" },
+      { protocol: "https", hostname: "**" },
     ],
   },
 };
